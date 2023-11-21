8e2d1223e664f875fd0c2ab43347cf85e7c9c9b9
Hellwin <elvinkauffmann@gmail.com>
2023-11-21 11:29:39 +0100

    pull test

--- elvin/src/mosaique/main.cpp
+++ elvin/src/mosaique/main.cpp
@@ -4,12 +4,17 @@
 int main()
 {
     sil::Image image{"images/logo.png"};
+    sil::Image imageIncrement {image.width()*2, image.height()*2};
 
     for (float y{0}; y < image.height(); y++)
     {
         for (float x{0}; x < image.width()/2; x++)
         {
-            std::swap(image.pixel(x, y), image.pixel(image.width() - (x+1), y) );
+            // sil::Image imageIncrement {imageIncrement.width()+image.width(), imageIncrement.height()+image.height()};
+            imageIncrement.pixel(x, y) = image.pixel(x, y);
+            imageIncrement.pixel(x + x*(image.width()-1), y + y*(image.height()-1)) = image.pixel(x, y);
+            imageIncrement.pixel(x, y + y*(image.height()-1)) = image.pixel(x, y);
+            imageIncrement.pixel(x + x*(image.width()-1), y) = image.pixel(x, y);
         }
     }
 
