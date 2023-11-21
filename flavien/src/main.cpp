c7c90c3f833d8d522b18f6e3d81dcbf897c5e65a
Flavien <flavien.izackovic@gmail.com>
2023-11-21 16:52:34 +0100

    init Exo 16 glitch

--- flavien/src/main.cpp
+++ flavien/src/main.cpp
@@ -504,3 +504,45 @@ for (int x2{0}; x2 <image3.width(); x2 += 300 )
 }
 
 */
+
+
+
+/* EXO 16 Glitch
+
+#include "random.hpp"
+
+int main()
+{
+    set_random_seed(3);
+    sil::Image image{"images/logo.png"};
+    sil:: Image image2{"images/logo.png"};
+    // TODO: modifier l'image
+    int Largeur {};
+    int Longueur {};
+    int x {};
+    int y {};
+
+    for (int a {0}; a < 100; a++)
+    {
+        x = random_int (1, 300);
+        y = random_int (2, 345);
+        Largeur = random_int (2, 150);
+        Longueur = random_int (2, 30);
+
+        
+        for (int i {x}; i < Largeur + x; i++)
+            {
+            for (int j{y}; j < Longueur + y; j++)
+                {
+                if ((i+Longueur < 300 ) && (j+Longueur < 345))
+                    { 
+                    image2.pixel(i,j) = image.pixel(i+Longueur, j+Longueur);
+                    }
+                }
+            }
+        
+    }
+    image2.save("output/pouet.png");
+}
+
+*/
