5d0f5cf95364afb653d831544a241b93293d02c7
Hellwin <elvinkauffmann@gmail.com>
2023-11-21 12:25:54 +0100

    mosaique semi fonctionnelle

--- elvin/src/mosaique/main.cpp
+++ elvin/src/mosaique/main.cpp
@@ -3,20 +3,24 @@
 
 int main()
 {
+    int nbrepeat {3};
     sil::Image image{"images/logo.png"};
-    sil::Image imageIncrement {image.width()*2, image.height()*2};
+    sil::Image imageIncrement {image.width()*nbrepeat, image.height()*nbrepeat};
 
-    for (float y{0}; y < image.height(); y++)
+    // imageIncrement.pixel(x, y) = image.pixel(x, y);
+
+    for (size_t i = 0; i < nbrepeat; i++)
     {
-        for (float x{0}; x < image.width()/2; x++)
+        for (float y{0}; y < image.height(); y++)
         {
-            // sil::Image imageIncrement {imageIncrement.width()+image.width(), imageIncrement.height()+image.height()};
-            imageIncrement.pixel(x, y) = image.pixel(x, y);
-            imageIncrement.pixel(x + x*(image.width()-1), y + y*(image.height()-1)) = image.pixel(x, y);
-            imageIncrement.pixel(x, y + y*(image.height()-1)) = image.pixel(x, y);
-            imageIncrement.pixel(x + x*(image.width()-1), y) = image.pixel(x, y);
+            for (float x{0}; x < image.width(); x++)
+            {
+                imageIncrement.pixel(x + i*(image.width()-1), y + i*(image.height()-1)) = image.pixel(x, y);
+                imageIncrement.pixel(x, y + i*(image.height()-1)) = image.pixel(x, y);
+                imageIncrement.pixel(x + i*(image.width()-1), y) = image.pixel(x, y);
+            }
         }
     }
 
-    image.save("output/pouet.png");
+    imageIncrement.save("output/pouet.png");
 }
