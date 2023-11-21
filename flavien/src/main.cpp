c9db1c775efab06c00770cbe09dca85e5d8bb538
Flavien <flavien.izackovic@gmail.com>
2023-11-21 14:30:10 +0100

    Main avec exo mosaique

--- flavien/src/main.cpp
+++ flavien/src/main.cpp
@@ -405,19 +405,37 @@ for (int x{0}; x < image.width(); x++)
 }
 */
 
+
+
+
+
+/* EXO 14 MOSAÏQUE 
 int main ()
 {
-    sil::Image image{500, 500};
-     
+    sil::Image image1{"images/logo.png"};
+    sil::Image image2{1500, 1725};
+    
     // TODO: modifier l'image
- 
-for (int x{0}; x < image.width(); x++)
-{
-    for (int y{0}; y < image.height(); y++)
-    {   
+for (int x2{0}; x2 <image2.width(); x2 += 300 )
+    {
+        for (int y2{0}; y2<image2.height(); y2 += 345 )
+        {
+            for (int x1{x2}; x1 < image1.width() + x2 ; x1++)
+                {
+                    for (int y1{y2}; y1 < image1.height() +y2 ; y1++)
 
-    }
-}
+                    {
+                        image2.pixel(x1,y1).r = image1.pixel(x1 - x2,y1 - y2).r;
+                        image2.pixel(x1,y1).g = image1.pixel(x1 - x2,y1 - y2).g;
+                        image2.pixel(x1,y1).b = image1.pixel(x1 - x2,y1 - y2).b;
+                    }
+                }        
+                
+        }   
+    }   
 
-    image.save("output/pouet.png");
+    image2.save("output/pouet.png");
 }
+*/
+
+
